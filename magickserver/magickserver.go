133c02d318ee9b7941bb15e1003c24ce7ebb3b7e
Matthias Luedtke <github@matthias-luedtke.de>
2015-10-07 19:01:23 +0200

    Add magickserver

---
+++ magickserver/magickserver.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/mat/magick"
+
+	// Needed for format detection:
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+)
+
+func imgHandler(w http.ResponseWriter, r *http.Request) {
+	imgURL := r.FormValue("url")
+	if imgURL == "" {
+		http.Error(w, "missing parameter: url", 401)
+		return
+	}
+
+	size := r.FormValue("size")
+	if size == "" {
+		http.Error(w, "missing parameter: size", 401)
+		return
+	}
+
+	imgReponse, err := http.Get(imgURL)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not fetch image: %s", err), 400)
+		return
+	}
+
+	imgBytes, err := ioutil.ReadAll(imgReponse.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not read image data: %s", err), 400)
+		return
+	}
+	defer imgReponse.Body.Close()
+
+	_, imgFormat, err := image.DecodeConfig(bytes.NewReader(imgBytes))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not detect image format: %s", err), 501)
+		return
+	}
+	fmt.Println("format: ", imgFormat)
+
+	now := time.Now()
+	img, err := magick.NewFromBlob(imgBytes, imgFormat)
+	defer img.Destroy()
+	err = img.Resize(size)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("resize failed: %s", err), 501)
+		return
+	}
+
+	newImgBytes, err := img.ToBlob(imgFormat)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("encoding failed: %s", err), 501)
+		return
+	}
+	duration := time.Since(now)
+	fmt.Println("Resize took ", duration)
+
+	w.Header().Set(contentType, fmt.Sprintf("image/%s", imgFormat))
+	w.Header().Set(xResize, fmt.Sprintf("%v", duration))
+	w.Write(newImgBytes)
+}
+
+const contentType = "Content-Type"
+const xResize = "X-Resize"
+
+func main() {
+	http.HandleFunc("/img", imgHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
