0c0e0e8e9a2bd9dc2cff7f781113ff992059a1ef
Matthias Luedtke <github@matthias-luedtke.de>
2015-10-08 19:54:10 +0200

    A little cleanup

--- magickserver/magickserver.go
+++ magickserver/magickserver.go
@@ -12,77 +12,16 @@ import (
 
 	"github.com/mat/magick"
 
-	// Needed for format detection:
+	// Supported image formats:
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 )
 
-func imgHandler(w http.ResponseWriter, r *http.Request) {
-	imgURL := r.FormValue("url")
-	if imgURL == "" {
-		http.Error(w, "missing parameter: url", 401)
-		return
-	}
-
-	size := r.FormValue("size")
-	if size == "" {
-		http.Error(w, "missing parameter: size", 401)
-		return
-	}
-
-	getStarted := time.Now()
-	imgReponse, err := http.Get(imgURL)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Could not fetch image: %s", err), 400)
-		return
-	}
-	getDuration := time.Since(getStarted)
-
-	imgBytes, err := ioutil.ReadAll(imgReponse.Body)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Could not read image data: %s", err), 400)
-		return
-	}
-	defer imgReponse.Body.Close()
-
-	detectionStarted := time.Now()
-	_, imgFormat, err := image.DecodeConfig(bytes.NewReader(imgBytes))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("could not detect image format: %s", err), 501)
-		return
-	}
-	detectionDuration := time.Since(detectionStarted)
-
-	resizeStarted := time.Now()
-	img, err := magick.NewFromBlob(imgBytes, imgFormat)
-	defer img.Destroy()
-	err = img.Resize(size)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("resize failed: %s", err), 501)
-		return
-	}
-
-	newImgBytes, err := img.ToBlob(imgFormat)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("encoding failed: %s", err), 501)
-		return
-	}
-	resizeDuration := time.Since(resizeStarted)
-	log.Println("get=", getDuration, ", detection=", detectionDuration, ", resize=", resizeDuration)
-
-	w.Header().Set(contentType, fmt.Sprintf("image/%s", imgFormat))
-	w.Header().Set(xTimings, fmt.Sprintf("get=%v, resize=%v", getDuration, resizeDuration))
-
-	w.Write(newImgBytes)
-}
-
-const contentType = "Content-Type"
-const xTimings = "X-Image-Timings"
-
 func main() {
+	http.HandleFunc("/", imgHandler)
+
 	log.Println("Starting server...")
-	http.HandleFunc("/img", imgHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("cannot start, need a PORT")
@@ -91,3 +30,100 @@ func main() {
 	log.Println("Server running on port", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
+
+func imgHandler(w http.ResponseWriter, r *http.Request) {
+	imgURL := r.FormValue("url")
+	if imgURL == "" {
+		http.Error(w, "missing parameter: url (to image)", 401)
+		return
+	}
+	size := r.FormValue("size")
+	if size == "" {
+		http.Error(w, "missing parameter: size (300x300, 50%)", 401)
+		return
+	}
+
+	getStarted := time.Now()
+	imgBytes, e := getImageData(imgURL)
+	getDuration := time.Since(getStarted)
+	if e != nil {
+		httpError(w, e)
+		return
+	}
+
+	imgFormat, e := detectFormat(imgBytes)
+	if e != nil {
+		httpError(w, e)
+		return
+	}
+
+	resizeStarted := time.Now()
+	newImgBytes, e := resizeImage(imgBytes, imgFormat, size)
+	resizeDuration := time.Since(resizeStarted)
+	if e != nil {
+		httpError(w, e)
+		return
+	}
+
+	log.Printf("get=%s resize=%s\n", getDuration, resizeDuration)
+
+	w.Header().Set(contentType, fmt.Sprintf("image/%s", imgFormat))
+	w.Header().Set(xTimings, fmt.Sprintf("get=%v, resize=%v", getDuration, resizeDuration))
+
+	w.Write(newImgBytes)
+}
+
+func getImageData(imgURL string) ([]byte, *serverError) {
+	imgReponse, err := http.Get(imgURL)
+	if err != nil {
+		return nil, &serverError{fmt.Sprintf("could not fetch image: %s", err), 400}
+	}
+
+	imgBytes, err := ioutil.ReadAll(imgReponse.Body)
+	if err != nil {
+		return nil, &serverError{fmt.Sprintf("could read image data: %s", err), 400}
+	}
+	defer imgReponse.Body.Close()
+
+	return imgBytes, nil
+}
+
+func detectFormat(imgBytes []byte) (string, *serverError) {
+	_, imgFormat, err := image.DecodeConfig(bytes.NewReader(imgBytes))
+	if err != nil {
+		return "", &serverError{fmt.Sprintf("could not detect image format: %s", err), 501}
+	}
+	return imgFormat, nil
+}
+
+func resizeImage(imgBytes []byte, imgFormat string, size string) ([]byte, *serverError) {
+	img, err := magick.NewFromBlob(imgBytes, imgFormat)
+	defer img.Destroy()
+	if err != nil {
+		return nil, &serverError{fmt.Sprintf("init failed: %s", err), 501}
+	}
+
+	err = img.Resize(size)
+	if err != nil {
+		return nil, &serverError{fmt.Sprintf("resize failed: %s", err), 501}
+	}
+
+	newImgBytes, err := img.ToBlob(imgFormat)
+	if err != nil {
+		return nil, &serverError{fmt.Sprintf("encoding failed: %s", err), 501}
+	}
+
+	return newImgBytes, nil
+}
+
+func httpError(w http.ResponseWriter, e *serverError) {
+	http.Error(w, e.message, e.status)
+}
+
+type serverError struct {
+	message string
+	status  int
+}
+
+const contentType = "Content-Type"
+const xTimings = "X-Image-Timings"
